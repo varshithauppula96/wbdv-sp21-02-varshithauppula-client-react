67b7a2b491604e72316f0b06bbe0518a0e112d4d
Varshitha Uppula <varshithauppula@gmail.com>
2021-02-20 12:20:09 -0500

    editor added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import CourseManager from "./components/course-manager";
+import CourseEditor from "./components/course-editor";
 function App() {
   return (
     <div>
      <CourseManager/>
+     <CourseEditor/>
     </div>
   );
 }
---
+++ src/components/course-editor.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const CourseEditor =() =>{
+    return(
+        <h1>Hello from Course Editor</h1>
+    )
+}
+export default CourseEditor
