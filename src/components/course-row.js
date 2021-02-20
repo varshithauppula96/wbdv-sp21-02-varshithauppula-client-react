a94ce667bfe00527f8299b7feb2f5f348a246261
Varshitha Uppula <varshithauppula@gmail.com>
2021-02-20 13:19:07 -0500

    table added

--- src/components/course-row.js
+++ src/components/course-row.js
@@ -1,18 +1,20 @@
 import React from 'react';
 
-const CourseRow =()=>{
-    return(
-        <div>
-            <table className="table">
-                <tr>
-                    <td>CS5610</td>
-                    <td>ME</td>
+const CourseRow = ( {course,
+                        title ="webdev",
+                        owner="me",
+                        lastModified="1/3/2020"
+}) =>
+    <tr>
+                    <td>{course.title}</td>
+                    <td>{course.owner}</td>
+                    <td>{course.lastModified}</td>
                     <td ><i className=" float-right fa fa-trash"> </i>
                         <i className=" float-right fas fa-edit"> </i>
                         <i className=" float-right fas fa-check"> </i> </td>
                 </tr>
-            </table>
-        </div>
-    )
-}
+
+
+
+
 export default CourseRow
--- src/components/course-table.js
+++ src/components/course-table.js
@@ -2,15 +2,30 @@ import React from 'react';
 import CourseRow from "./course-row";
 
 export default class CourseTable extends React.Component {
+state ={
+    courses : [
+        {title:"CS5610", owner:"me", lastModified:"01/02/2021"},
+        {title:"CS5610", owner:"me", lastModified:"01/02/2021"},
+        {title:"CS5610", owner:"me", lastModified:"01/02/2021"},
+        {title:"CS5610", owner:"me", lastModified:"01/02/2021"},
+        {title:"CS5610", owner:"me", lastModified:"01/02/2021"},
+        {title:"CS56800",owner:"me", lastModified:"01/02/2021"},
+    ]
+}
+
+
+
     render() {
         return (
             <div>
             <h2>Course Table</h2>
-            <CourseRow/>
-                <CourseRow/>
-                <CourseRow/>
-                <CourseRow/>
-                <CourseRow/>
+                <table className="table">
+                    {
+                        this.state.courses.map( course =>
+                            <CourseRow course={course}/>)
+                    }
+                    </table>
+
             </div>
         )
 
