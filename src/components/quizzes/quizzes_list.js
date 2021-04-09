0ba68d360acd754574645c0e812ad56228727b7c
Varshitha Uppula <varshithauppula@gmail.com>
2021-04-09 11:30:43 -0400

    quizzes seperate services file

--- src/components/quizzes/quizzes_list.js
+++ src/components/quizzes/quizzes_list.js
@@ -1,15 +1,22 @@
 import React, {useState,useEffect} from "react"
-
+import quizService from "../services/quiz-service"
 const QuizzesList =() =>{
     const [quizzes,setQuizzes] =useState([])
     useEffect(() =>{
-fetch("http://localhost:3000/api/quizzes")
-    .then(response => response.json())
-    .then((quizzes)=>{setQuizzes(quizzes)})
-    },[])
+// fetch("http://localhost:3000/api/quizzes")
+//     .then(response => response.json())
+//     .then((quizzes)=>{setQuizzes(quizzes)
+
+            quizService.findAllQuizzes().then((quizzes) => {setQuizzes(quizzes)})
+    }, [])
     return(
         <div>
             <h1>Quizzes list</h1>
+    <ul className="list-group"> {quizzes.map((quiz) => {
+        return(
+            <li className="list-group-item">{quiz.title}</li>
+        )
+    })}</ul>
         </div>
     )
 }
